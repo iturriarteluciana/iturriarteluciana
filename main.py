60c4bede4087a9f83bb5e05c23bf8ef42ea1795a
iturriarteluciana <iturriarteluciana@gmail.com>
2024-10-17 20:56:48 +0000

    jjj

--- main.py
+++ main.py
@@ -1,1 +1,16 @@
-
+nombre='luciana'
+numero=3
+booleano=True
+booleano=False
+print(numero)
+print(3)
+edadDeValery=16
+puedeVotar=edadParaVotar<=edadDeValery
+if puedeVotar
+   print("puede votar")
+contador=0
+condicion=True
+while condicion:if
+ contador=contador+1
+ condicion=contador<10
+ print(contador)
