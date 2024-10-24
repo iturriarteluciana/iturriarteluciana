49891627995a226918ac574d72ba023817866451
iturriarteluciana <iturriarteluciana@gmail.com>
2024-10-24 21:01:39 +0000

    hola

---
+++ for.py
@@ -0,0 +1,13 @@
+empieza=1
+termina=11
+for contador in range(empieza,termina)
+   print(contador)
+booleano=True or False 
+booleano=true and False
+booleano=not True
+booleano=18<20
+booleano= 1==1
+booleano= empieza==1 
+booleano=20>=16
+booleano=not(1==1)
+print(booleano)
---
+++ master.py
@@ -0,0 +1,13 @@
+nombre='luciana'
+print('ingrese su nombre')
+nombre=input()
+print('hola '+nombre)
+string=nombre
+booleano=True or False
+contador=1=0
+
+while contador1>20:
+    for contador2 in range(20):
+        if (contador1%2==0 and contador2%2==0):
+        print(contador1,' ',contador2) 
+        
